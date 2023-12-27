a43c2901305277a669528cdda3170dac715d1411
Gabriel Karlsson <gabriel-carlsson@hotmail.com>
2023-12-27 18:25:52 +0100

    test done

--- TodoWebApi.Tests/NoteProcessorTest.cs
+++ TodoWebApi.Tests/NoteProcessorTest.cs
@@ -33,5 +33,49 @@ namespace TodoWebApi.Tests
 			Assert.NotNull(postNote);
 			Assert.Single(postNote);
 		}
+
+		[Fact]
+		public void Should_Get_List_Of_Notes()
+		{
+			//ARRANGE
+			var post1 = new Note
+			{
+				Text = "Shop",
+				IsDone = false
+			};
+
+			var post2 = new Note
+			{
+				Text = "Study",
+				IsDone = false
+			};
+
+			var post3 = new Note
+			{
+				Text = "Clean house",
+				IsDone = false
+			};
+
+			//ACT
+			_processor.Post(post1);
+			_processor.Post(post2);
+			_processor.Post(post3);
+
+			List<Note> getNotes = _processor.GetAllNotes();
+
+			//ASSERT
+			Assert.NotNull(getNotes);
+			Assert.Equal(3, getNotes.Count);
+		}
+
+		[Fact]
+		public void Should_Get_Empty_List()
+		{
+			//ACT
+			List<Note> getNotes = _processor.GetAllNotes();
+
+			//ASSERT
+			Assert.Empty(getNotes);
+		}
 	}
 }
